3c5654066283535a0562d9b8d46ce27da3ba2b53
RockyEducacion <rocky.edu@capcol.com.co>
2026-04-28 19:58:09 -0500

    2.3.0.

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,4 +1,4 @@
 export const SUPABASE_URL = 'https://rknphdxrtcftttmapbkx.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrbnBoZHhydGNmdHR0bWFwYmt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNzM1NjEsImV4cCI6MjA4ODg0OTU2MX0.cHZbEyuflz53KlWWkamxhzpYxUDzFx6svtS2nyUSv3M';
 export const SUPABASE_PROFILES_TABLE = 'profiles';
-export const EMPLOYEE_PORTAL_API_BASE = 'https://https://rockyeducacion-whatsapp.vercel.app/';
+export const EMPLOYEE_PORTAL_API_BASE = 'https://rockyeducacion-whatsapp.vercel.app/';
